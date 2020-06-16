94f2d1d530693efb04e5eb4199acee16e4349b91
oliver <oliver@freiheit-kiel.de>
2020-06-16 17:57:20 +0200

    first commit

---
+++ expedition-sysBool.py
@@ -0,0 +1,41 @@
+###Expedition System Boolean
+#Skript to check the SystemBooleans
+from ctypes import *
+from enum import Enum
+import winreg
+import json
+import psutil
+
+class SystemBoolean(Enum):
+    UTC = 2
+    Mag = 4
+    Quit = 24
+    UserFlag = 44
+
+REG_PATH = 'SOFTWARE\Expedition\Core'
+def get_dll_location():
+    try:
+        registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0,
+                                       winreg.KEY_READ)
+        value, regtype = winreg.QueryValueEx(registry_key, 'Location')
+        winreg.CloseKey(registry_key)
+        return value
+    except WindowsError:
+        return None
+dll_location = get_dll_location()
+    git rebase -i HEAD~4
+expedition = windll.LoadLibrary(dll_location +'\ExpDLL.dll')
+
+def get_sys_bool(sys_channel, boat_id): 
+    results_bool = c_bool()
+    expedition.GetSysBool(c_short(sys_channel.value), pointer(results_bool))
+    print (str(results_bool.value))
+    return results_bool.value
+i = 0
+i = i+1
+boat_id = 0
+results_bool = {}
+
+for channel in SystemBoolean:
+    results_bool[channel.name] = get_sys_bool(channel, boat_id)
+    print (results_bool)
